0abea60bbc7c59d7da0048c7f6f98582c72ff7c0
Cave Smith <dracothedragon9292@gmail.com>
2026-09-17 17:44:09 -0600

    fixing an issue

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="bg-gray-800 text-white p-4">
-      <div id="header-title" className="text-2xl font-bold">
+      <h1 id="header-title" className="text-2xl font-bold">
         Thomas Barney
-      </div>
+      </h1>
       <nav className="container mx-auto flex justify-between items-center">
         <ul className="flex gap-6">
           <li>
